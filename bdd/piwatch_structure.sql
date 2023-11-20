ba59611976520777017dca0979494f33b219445d
sabiab00 <sabiab00@estudiantes.unileon.es>
2023-11-20 11:25:48 +0100

    Base de Datos

---
+++ bdd/piwatch_structure.sql
@@ -0,0 +1,36 @@
+CREATE EXTENSION IF NOT EXISTS "piwatch";
+
+CREATE TABLE "USER"
+(
+    "IDUSER"                   uuid DEFAULT uuid_generate_v4(),
+    "NAMEUSER"                 VARCHAR(100) NOT NULL,
+    "PASSWORDUSER"             VARCHAR(100) NOT NULL,
+    "TIMESTAMPUSER"            TIMESTAMP    NOT NULL
+    PRIMARY KEY ("ID")
+);
+
+CREATE TABLE "LOGINATTEMPT"
+(
+    "IDLOGIN"                   uuid DEFAULT uuid_generate_v4(),
+    "NAMELOGIN"                 VARCHAR(100) NOT NULL,
+    "PASSWORDLOGIN"             VARCHAR(100) NOT NULL,
+    "TIMESTAMPLOGIN"           TIMESTAMP    NOT NULL,
+    "IP"                        VARCHAR(20)  NOT NULL
+    PRIMARY KEY ("ID")
+);
+
+CREATE TABLE "INCIDENTS"
+(
+    "IDINCIDENT"               uuid DEFAULT uuid_generate_v4(),
+    "TIMESTAMPINCIDENTS"       INT NOT NULL,
+    "INCIDENTPIC"              VARCHAR(50) DEFAULT NULL,
+    PRIMARY KEY ("IDINCIDENT")
+)
+
+CREATE TABLE "WATCHSCHEDULE" (
+  "IDSCHEDULE"                uuid DEFAULT uuid_generate_v4(),
+  "SCHEDULESTART"             TIMESTAMP NOT NULL,
+  "SCHEDULEEND"               TIMESTAMP NOT NULL,
+  "SCHEDULEUSER"              INT DEFAULT NULL,
+  PRIMARY KEY ("SCHEDULEID")
+)

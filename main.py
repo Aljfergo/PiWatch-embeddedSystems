4d6cb6e37765f0ec83a420d8843e033c89b30f5c
dalcoa00 <dalcoa00@estudiantes.unileon.es>
2023-11-24 17:24:15 +0100

    Sentencias hechas, funcionamiento no comprobado

--- main.py
+++ main.py
@@ -37,6 +37,11 @@ class Schedule(BaseModel):
     scheduleStart: str
     scheduleEnd: str
 
+class Incident(BaseModel):
+    title: str
+    description: str
+    severity: str
+
 
 
 
@@ -79,6 +84,118 @@ def connect():
 #
 #   ->  POSTS
 #
+
+    #   post de usuario (registro - sign in)
+@app.post("/signin")
+async def registerUser(userCredentials: UserCredentials):
+    print("Se ha recibido la solicitud de registro del usuario " + userCredentials.username)
+    
+    # Verificar si el usuario ya existe
+    existing_user = check_user_exists(userCredentials.username)
+    
+    if existing_user:
+        raise HTTPException(status_code=400, detail="El usuario ya existe. Elija otro nombre de usuario.")
+    
+    # Insertar el nuevo usuario en la base de datos
+    insert_user(userCredentials.username, userCredentials.password)
+    
+    return {"mensaje": "Usuario registrado exitosamente"}
+
+def check_user_exists(username):
+    sentenciaSQL = "SELECT * FROM users WHERE username = %s"
+    conn = None
+    
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+
+        cur.execute(sentenciaSQL, (username,))
+        existing_user = cur.fetchone()
+
+        return existing_user is not None
+    
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        raise HTTPException(status_code=500, detail="Error en la base de datos al verificar la existencia del usuario")
+    
+    finally:
+        if conn is not None:
+            conn.close()
+
+def insert_user(username, password):
+    sentenciaSQL = "INSERT INTO users (username, password) VALUES (%s, %s)"
+    conn = None
+    
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+
+        cur.execute(sentenciaSQL, (username, password))
+        conn.commit()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        raise HTTPException(status_code=500, detail="Error en la base de datos al registrar el usuario")
+
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+
+        #post de intento de registro (subir un intento de inicio de sesión) ????????
+        #
+        #
+        #
+        #
+        #
+        #
+        #
+        #
+        #
+        #
+        #
+        #
+        #
+        #
+        #
+        #
+        #
+        
+        
+        #post de incidentes
+@app.post("/incidents")
+async def create_incident(incident: Incident):
+    print("Se ha recibido un nuevo incidente: " +incident.title)
+
+    # Insertar el incidente en la base de datos
+    insert_incident(incident.title, incident.description, incident.severity)
+
+    return {"mensaje": "Incidente registrado exitosamente"}
+
+def insert_incident(title, description, severity):
+    sentenciaSQL = "INSERT INTO incidents (title, description, severity) VALUES (%s, %s, %s)"
+    conn = None
+
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+
+        cur.execute(sentenciaSQL, (title, description, severity))
+        conn.commit()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        raise HTTPException(status_code=500, detail="Error en la base de datos al registrar el incidente")
+
+    finally:
+        if conn is not None:
+            conn.close()
+
+
 
     #   check usuario-contraseña
 @app.post("/login")
@@ -166,6 +283,35 @@ async def checkData():
             conn.close()
 
 
+    #TO-DO
+    
+    #   get de incidentes //////////////////////////////////////////// NO ENTIENDO BIEN QUÉ HAY QUE HACER AQUÍ
+@app.get("/incidents")
+async def checkData():
+    print("Se han solicitado los incidentes del sistema")
+    sentenciaSQL="""SELECT * FROM INCIDENTES""" #Hay algún filtro para los incidentes??
+    conn = None
+    try:
+        params=config()
+        conn =psycopg2.connect(**params)
+        cur=conn.cursor()
+
+        cur.execute(sentenciaSQL)
+        user= cur.fetchone()
+        cur.close()
+
+        if user:
+            return {"mensaje":"Credenciales válidas"}
+        else:
+            return HTTPException(status_code=401, detail="Credenciales inválidas")
+        
+    except (Exception, psycopg2.DatabaseError)  as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
 
 #========================#
 #     Actualizaciones    #
@@ -175,7 +321,72 @@ async def checkData():
 #   ->  PUTS
 #
 
-#   Edición de los horarios de vigilancia
+    #   Edición de los horarios de vigilancia
+@app.put("/{user}/schedule")
+async def checkPassword(schedule : Schedule):
+    print("Se ha recibido una edición de horario por parte de: "+ user)
+    sentenciaSQL="""UPDATE "WATCHSCHEDULE" 
+                        WHERE SCHEDULEUSER= %s
+                        SET SCHEDULESTART = %s,
+                        SET SCHEDULEEND = %s"""
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+
+        cur.execute(sentenciaSQL, (schedule.scheduleStart, schedule.scheduleEnd, user))
+        conn.commit() 
+
+        cur.close()
+
+        return {"mensaje": "Horario actualizado correctamente"}
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        raise HTTPException(status_code=400, detail="Error al editar el horario")
+
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+
+
+#======================#
+#     Eliminaciones    #
+#======================#
+
+#
+#   ->  DELETES
+#
+
+    #   Eliminar un horario ¿del usuario que ha realizado la acción?
+@app.delete("/{user}/schedule")
+async def checkPassword(schedule : Schedule):
+    print("Se ha recibido una eliminación de horario por parte de: "+ user)
+    sentenciaSQL="""DELETE FROM "WATCHSCHEDULE" 
+                        WHERE SCHEDULEUSER= %s"""
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+
+        cur.execute(sentenciaSQL, (user,))
+        conn.commit() 
+
+        cur.close()
+
+        return {"mensaje": "Horario eliminado correctamente"}
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        raise HTTPException(status_code=400, detail="Error al eliminar el horario")
+
+    finally:
+        if conn is not None:
+            conn.close()
 
 
 

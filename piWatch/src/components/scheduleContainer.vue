fce5462293a8a2f3c274669496ebadd63eeaf253
Aljfergo <72195815+Aljfergo@users.noreply.github.com>
2023-12-08 23:15:39 +0100

    schedule activation

--- piWatch/src/components/scheduleContainer.vue
+++ piWatch/src/components/scheduleContainer.vue
@@ -1,33 +1,54 @@
 <template>
-    <v-sheet class="card-container-sheet">
-     <v-container fluid>
-       <v-row>
-         <v-row v-for="(schedule, index) in schedules" :key="index" cols="12" md="4">
-           <v-card>
-             <v-card-title>{{ schedule.scheduleEnd }}</v-card-title>
-             <v-card-title>{{ schedule.scheduleStart }}</v-card-title>
-             <v-button>Activar horario</v-button>
-           </v-card>
-         </v-row>
-       </v-row>
-     </v-container>
-    </v-sheet>
-   </template>
-   
-   <script>
-   export default {
-     props: {
-       schedules: {
-         type: Array,
-         required: true,
-       },
-     },
-   };
-   </script>
-   
-   <style scoped>
-   .card-container-sheet {
-     max-height: 500px; 
-     overflow-y: auto; 
-   }
-   </style>
+  <v-sheet class="card-container-sheet">
+    <v-container fluid>
+      <v-row>
+        <v-row v-for="(schedule, index) in schedules" :key="index" cols="12" md="4">
+          <v-card>
+            <v-card-title>{{ schedule.id }}</v-card-title>
+            <v-card-title>{{ schedule.scheduleEnd }}</v-card-title>
+            <v-card-title>{{ schedule.scheduleStart }}</v-card-title>
+            <v-btn @click="enableSchedule(schedule.id)">Activar horario</v-btn>
+          </v-card>
+        </v-row>
+      </v-row>
+    </v-container>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  props: {
+    schedules: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    enableSchedule(scheduleId) {
+      fetch(`/api/schedule/${scheduleId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          console.log('Schedule activado correctamente');
+        })
+        .catch(error => {
+          console.error('Error al activar el horario:', error);
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.card-container-sheet {
+  max-height: 500px;
+  overflow-y: auto;
+}
+</style>

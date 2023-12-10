f0d44a8e084d4f2bf3620a776fd2eb290de0e4a8
Aljfergo <72195815+Aljfergo@users.noreply.github.com>
2023-12-10 14:12:34 +0100

    arreglados problemas con el token, y cambios en el bot. Subidos en el Drive los diagramas hardware y software.

--- piWatch-alertBOT/tokenGen.py
+++ piWatch-alertBOT/tokenGen.py
@@ -1,19 +1,19 @@
 import telepot
 from telepot.loop import MessageLoop
-from token import token
 
-
-TOKEN = '6785587695:AAH0tjLxIIlGxuaUaJIXewNlkQO2HI9MeT4'
+# Leer el token desde el archivo
+with open('../token.txt', 'r') as file:
+    TOKEN = file.read().strip()
 
 def handle_start(msg):
     chat_id = msg['chat']['id']
-    bot.sendMessage(chat_id, f'Bienvenido a PiWatch! Por favor, inserte el siguiente token en el registro para poder vincular sus cuentas.')
+    bot.sendMessage(chat_id, '¡Bienvenido a PiWatch!')
+    bot.sendMessage(chat_id, 'Por favor, inserte el siguiente token en el registro para poder vincular sus cuentas.')
     bot.sendMessage(chat_id, f'Tu token único es: {chat_id}')
 
 bot = telepot.Bot(TOKEN)
 MessageLoop(bot, {'chat': handle_start}).run_as_thread()
 print('Escuchando...')
-
 
 while True:
     pass
